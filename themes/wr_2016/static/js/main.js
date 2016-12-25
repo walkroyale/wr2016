b88b01422167e27a3725f85332f92ff9246fc29a
RJ Lewis <justrjlewis@gmail.com>
2016-12-25 15:53:49 -0600

    v0.8.0 main product styles and function are working.  @todo add content and admin inteface

--- themes/wr_2016/static/js/main.js
+++ themes/wr_2016/static/js/main.js
@@ -1,44 +1,69 @@
 jQuery(document).ready(function(){
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    // direction: 'horizontal',
-    loop: true,
+  //change pic
+    var galleryContainers = document.querySelectorAll('.single-product');
 
-    // If we need pagination
-    // pagination: '.swiper-pagination',
+    for ( var i=0; i < galleryContainers.length; i++ ) {
+      initGalleryContainer( galleryContainers[i] );
+    }
 
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+    function initGalleryContainer( container ) {
+      // init flickity
+      var galleryElem = container.querySelector('.t-product-image__slider');
+      var flkty = new Flickity( galleryElem, {
+        cellAlign: 'left',
+        pageDots: false
+      });
+      // hook up navigation
+      var nav = container.querySelector('.product_opt__list');
+      // get array of items
+      var items = Array.prototype.slice.call( nav.children );
+      nav.addEventListener( 'click', function( event ) {
+        // only link clicks
+        // alert(items);
+        if ( !matchesSelector( event.target, 'a' ) ) {
+          return;
+        }
+        event.preventDefault();
+        var item = event.target.parentNode;
+        var index =  items.indexOf( item );
+        flkty.select( index );
+      });
 
-    // slidesPerView: 1,
-    // paginationClickable: true,
-    grabCursor: true,
-    hashnav: true,
-    slideToClickedSlide:true
-    // spaceBetween: 10
-   });
-  $(".new_select").select2({
-    minimumResultsForSearch: Infinity
-  });
-  // $(".product_select").select2({
-  //   minimumResultsForSearch: Infinity
-  //   // theme: "product_select"
-  // });
-  // var $eventLog = $(".js-event-log");
-  // var $e = $("<span>" + currentVal + "</span>");
-  // $eventLog.append($e);
+      $carousel = $('.t-product-image__slider').flickity();
+      var $cellWrapper = $('.product_opt__list');
+      var $cellButtons = $cellWrapper.find('li a');
+
+      // update selected cellButtons
+      $carousel.on( 'select.flickity', function() {
+        $cellButtons.filter('.is-selected')
+          .removeClass('is-selected');
+        $cellButtons.eq( flkty.selectedIndex )
+          .addClass('is-selected');
+        
+        var $cartOpt = $cellButtons.filter('.is-selected').text();
+        
+        $cellButtons.filter('.is-selected').closest('.t-product-options').find('.snipcart-add-item').data('item-custom2-value', $cartOpt);
+        stopPropagation();
+      });
+    }
+
+  //select box
+    $("select").selectBoxIt({
+      //hide the currently selected opt
+      hideCurrent: true
+    });
 
   // item color
     var $colorSelect = $(".product_select.color");
 
-    $colorSelect.on("change", function (e) {
-      var currentVal = $(this).val();
-    
+    $colorSelect.on("click", function (e) {
+      var currentVal = $(this).text();
+      // alert(currentVal);
       e.preventDefault();
-      
+      // e.stopPropagation();
+      $(this).closest('ul').find('li a').removeClass('is-selected');
+      $(this).addClass('is-selected');
       $(this).closest('.t-product-options').find('.snipcart-add-item').data('item-custom2-value', currentVal);
-      e.stopPropagation();
     });
 
   // item size
